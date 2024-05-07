6c70aad13385da4497cb1826e7b18d94e29c9cc9
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-05-07 11:42:16 -0400

    added new files

---
+++ src/script.mjs
@@ -0,0 +1,12 @@
+//Part1: The scenario
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.mjs";
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+}
+console.log("work")
